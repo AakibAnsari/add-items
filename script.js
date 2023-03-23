93512f2c4294f9f039e06e4039bf973015c9fd56
AakibAnsari <aakiba6619@gmail.com>
2023-03-23 17:17:38 +0530

    simple add items

---
+++ script.js
@@ -0,0 +1,10 @@
+// const headerTitle= document.getElementById('header-title');
+// const header= document.getElementById('main-header');
+// header.style.borderBottom='solid 3px #000'
+
+var title= document.querySelector('.title');
+// console.log(items);
+// console.log(items[1]);
+// items[1].textContent="hello-2";
+title.style.fontweight='bold';
+title.style.color='green';
