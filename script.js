bca7cded5597b7d420a164ef86f64875e6cc5890
AakibAnsari <aakiba6619@gmail.com>
2023-03-23 17:52:38 +0530

    deleting 3rd item and green 2nd item

--- script.js
+++ script.js
@@ -17,10 +17,10 @@
 // items[3].style.fontWeight = "900";
 
 var li= document.getElementsByTagName('li');
-li[1].textContent="hello-2";
-li[1].style.fontWeight='bold';
-// li[1].style.background='green';
+// li[1].textContent="hello-2";
+li[2].style.display='none';
+li[1].style.background='green';
 
-for(var i=0; i<li.length;i++){
-    li[i].style.background='green';
-}
+// for(var i=0; i<li.length;i++){
+//     li[i].style.background='green';
+// }
